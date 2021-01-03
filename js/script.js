1c299abaad1c5e776cd855d67df5b2992ec85545
ajismaulanadotid <ajismaulana.id@gmail.com>
2021-01-03 10:12:50 +0700

    awesome

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const typedEl = document.querySelector("#typed");
 
 const typed = new Typed(typedEl, {
-  strings: ["Ajis Maulana.", "Multimedia", "Developer.", "a Beginner."],
-  typeSpeed: 100,
+  strings: [
+    "Ajis Maulana.",
+    "Multimedia",
+    "Content",
+    "Developer.",
+    "a Beginner",
+    "Freelancer.",
+  ],
+  typeSpeed: 80,
   showCursor: false,
   backDelay: 1000,
 });
